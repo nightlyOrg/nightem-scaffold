e1ad077f2d7c235ed8f57b9bfb3cbaa0eb7f2773
Joshua Slui <joshuaslui0203@gmail.com>
2024-04-09 20:41:32 +0200

    Add directories configuration

---
+++ config/directories.py
@@ -0,0 +1,30 @@
+"""
+Configuration file responsible for defining the directories
+for the bot to use. Generally, it is not recommended to modify
+these directories unless you know what you are doing. The bot
+will not function properly if these directories are not defined
+correctly.
+
+If you change these directories, you will need to restart the
+bot for the changes to take effect. Additionally, you should
+ensure the directory locations are correct.
+
+Read more about directory configuration in the Github wiki:
+https://github.com/NightlyOrg/Nightem-scaffold/wiki
+"""
+
+
+import sys
+
+if sys.platform == "win32":
+    splitter = "\\"
+else:
+    splitter = "/"
+
+
+class Directories:
+    migrations = f"database{splitter}migrations"
+    seeders = f"database{splitter}seeders"
+
+    def get_directory(self, directory):
+        return self.__getattribute__(directory)
